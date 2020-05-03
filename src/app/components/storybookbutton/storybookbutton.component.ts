f1e6503cdbe5d6d0081713e0cc4f37806a0b8d4a
Steve Keyen <stevekeyen@gmail.com>
2020-05-03 13:00:44 +0200

    added some stories, admin config title, some kendo buttons

---
+++ src/app/components/storybookbutton/storybookbutton.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-storybookbutton',
+  template: `
+    <button (click)="onClick.emit($event)">{{ text }}</button>
+  `,
+  styles: [
+    `
+      button {
+        border: 1px solid #eee;
+        border-radius: 3px;
+        background-color: #ffffff;
+        cursor: pointer;
+        font-size: 15px;
+        padding: 3px 10px;
+        margin: 10px;
+      }
+    `,
+  ],
+})
+export default class StorybookbuttonComponent implements OnInit {
+  @Input()
+  text = '';
+  @Output()
+  onClick = new EventEmitter<any>();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
